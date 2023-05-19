f5e55c13c2f47fcb54b01024e5d5891e0b8cdb44
pnjnlng <pan.jackson1@gmail.com>
2023-05-19 15:33:14 -0700

    Game Panel and Main Class added basic window

--- MainClass.java
+++ MainClass.java
@@ -1,5 +1,17 @@
-public class MainClass{
-    public static void main(String[] args){
+import javax.swing.JFrame;
+public class MainClass {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setResizable(false);
+        window.setTitle("Arevalators");
+
+        GamePanel gamePanel = new GamePanel();
+        window.add(gamePanel);
+
+        window.pack();
         
+        window.setLocationRelativeTo(null);
+        window.setVisible(true);
     }
 }
