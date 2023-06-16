50d6197dd34c8e9ebe158ecce20a18b6f31d7e32
pnjnlng <pan.jackson1@gmail.com>
2023-06-16 12:13:58 -0700

    Added some sprite properties

--- Entity/Player.java
+++ Entity/Player.java
@@ -74,16 +74,36 @@ public class Player extends Entity {
         BufferedImage image = null;
         switch(direction) {
             case "up":
+            if(spriteNum == 1) {
                 image = up1;
+            }
+            if(spriteNum == 2) {
+                image = up2;
+            }
                 break;
             case "left":
+            if(spriteNum == 1) {
                 image = left1;
+            }
+            if(spriteNum == 2) {
+                image = left2;
+            }
                 break;
             case "down":
+            if(spriteNum == 1) {
                 image = down1;
+            }
+            if(spriteNum == 2) {
+                image = down2;
+            }
                 break;
             case "right":
+            if(spriteNum == 1) {
                 image = right1;
+            }
+            if(spriteNum == 2) {
+                image = right2;
+            }
                 break;
         }
         g2.drawImage(image, x, y, gp.tileSize, gp.tileSize, null);
